b7b7f06e59290fd3f173a56ac6a1af35b385bb7b
sylvestredavid <david.sylvestre.24@gmail.com>
2026-07-01 21:24:01 +0200

    ajout validation options

--- rules/src/QuadriOptions.ts
+++ rules/src/QuadriOptions.ts
@@ -1,6 +1,11 @@
-import { OptionsSpec } from '@gamepark/rules-api'
+import { OptionsSpec, OptionsValidationError } from '@gamepark/rules-api'
+/**
+ * This is the options for each player in the game.
+ */
+type PlayerOptions = { id: number }
 
 export type QuadriOptions = {
+  players: PlayerOptions[]
   cooperative: boolean
   advancedCooperative: boolean
   balltrap: boolean
@@ -9,6 +14,13 @@ export type QuadriOptions = {
 }
 
 export const QuadriOptionsSpec: OptionsSpec<QuadriOptions> = {
+  players: {
+    id: {
+      label: (t) => t('player.id'),
+      values: [1, 2, 3, 4, 5, 6],
+      valueSpec: (id) => ({ label: (t) => t(`player.${id}`) })
+    }
+  },
   cooperative: {
     label: (t) => t('option.cooperative'),
     help: (t) => t('option.cooperative.help'),
@@ -34,5 +46,12 @@ export const QuadriOptionsSpec: OptionsSpec<QuadriOptions> = {
     help: (t) => t('option.advanced.help'),
     hide: (players) => players > 4
   },
-  competitivePlayers: { min: 2, max: 6 }
+  competitivePlayers: { min: 2, max: 6 },
+  validate: (options, t) => {
+    const players = options.players?.length ?? 0
+    const isCoop = (options.cooperative || options.advancedCooperative) ?? false
+    if (!isCoop && players > 4) {
+      throw new OptionsValidationError(t('more.than.4.players.require.coop'), ['players'])
+    }
+  }
 }
